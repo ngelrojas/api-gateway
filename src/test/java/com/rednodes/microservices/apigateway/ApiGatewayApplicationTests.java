2f806a724e95a459109d1b010973c0b06389a0a7
ngel rojas <aomarrojasp@gmail.com>
2023-03-29 18:26:15 -0300

    first commit

---
+++ src/test/java/com/rednodes/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rednodes.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
